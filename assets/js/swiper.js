a3a53cf19d60b3a9f1bd6a8262c861ae963f5888
Zahra Ebrahimi <zahra.ea90@gmail.com>
2023-08-23 21:10:10 +0330

    gear-up-section-mostly

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,7 +1,97 @@
+//air-max-sliders
 const air_max_swiper = new Swiper("#air-max .swiper", {
   // Optional parameters
   direction: "horizontal",
   slidesPerView: 3.07,
+  spaceBetween: 10,
+  loop: false,
+  grabCursor: true,
+  /*breakpoints: {
+    // when window width is >= 576px
+    576: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    // when window width is >= 992px
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    // when window width is >= 1200px
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    // when window width is >= 1400px
+    1400: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },*/
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+//gear-up-section-sliders:
+//1-mens-swiper
+const gear_up_mens_swiper = new Swiper("#mens-swiper .swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  slidesPerView: 2,
+  spaceBetween: 10,
+  loop: false,
+  grabCursor: true,
+  /*breakpoints: {
+    // when window width is >= 576px
+    576: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    // when window width is >= 992px
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    // when window width is >= 1200px
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    // when window width is >= 1400px
+    1400: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },*/
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+//2-women-swiper
+const gear_up_women_swiper = new Swiper("#women-swiper .swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  slidesPerView: 2,
   spaceBetween: 10,
   loop: false,
   grabCursor: true,
