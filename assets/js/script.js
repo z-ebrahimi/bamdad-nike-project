da74b3a45be5940ac3530111621514c2b89b9a57
Zahra Ebrahimi <zahra.ea90@gmail.com>
2023-08-28 00:18:38 +0330

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
+/*
+const searchBox = document.getElementById("header-search-box");
+const searchBoxInput = document.querySelector("#header-search-box input[type='text']");
 
-
-
+searchBoxInput.addEventListener("click",()=>{
+    searchBox.classList.add("active")
+})
+*/
