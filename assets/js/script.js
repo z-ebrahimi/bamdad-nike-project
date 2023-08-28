220e3022c43175ebe93a6505330e248c1b83d76a
Zahra Ebrahimi <zahra.ea90@gmail.com>
2023-08-28 07:26:44 +0330

    opened-search-box-and-responsive-changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,17 @@
-/*
-const searchBox = document.getElementById("header-search-box");
-const searchBoxInput = document.querySelector("#header-search-box input[type='text']");
+//header-opened-search-box
+const openedSearchBox = document.getElementById("header-opened-search-box");
+const searchBoxInput = document.querySelector(
+  "#header-search-box input[type='text']"
+);
+const openedSearchBoxInput = document.querySelector(
+    "#header-opened-search-box input[type='text']"
+  );
+const closeSearchBox = document.getElementById("close-search-box");
 
-searchBoxInput.addEventListener("click",()=>{
-    searchBox.classList.add("active")
-})
-*/
+searchBoxInput.addEventListener("click", () => {
+  openedSearchBox.classList.add("active");
+  openedSearchBoxInput.focus();
+});
+closeSearchBox.addEventListener("click", () => {
+  openedSearchBox.classList.remove("active");
+});
