43b620ad94cb2d040732d7ab3d75424613723f45
Zahra Ebrahimi <zahra.ea90@gmail.com>
2023-09-03 03:47:33 +0330

    usp-essentials-swipers

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -2,6 +2,51 @@
 const air_max_swiper = new Swiper("#air-max .swiper", {
   // Optional parameters
   direction: "horizontal",
+  slidesPerView: 1.2,
+  spaceBetween: 10,
+  loop: false,
+  grabCursor: true,
+  breakpoints: {
+    // when window width is >= 576px
+    576: {
+      slidesPerView: 1.2,
+      spaceBetween: 10,
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 2.2,
+      spaceBetween: 10,
+    },
+    // when window width is >= 992px
+    992: {
+      slidesPerView: 2.2,
+      spaceBetween: 10,
+    },
+    // when window width is >= 1200px
+    1200: {
+      slidesPerView: 3.1,
+      spaceBetween: 10,
+    },
+    // when window width is >= 1400px
+    1400: {
+      slidesPerView: 3.1,
+      spaceBetween: 10,
+    },
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+////////////////////////////
+//gear-up-section-sliders:
+//1-men-swiper
+const gear_up_men_swiper = new Swiper("#men-swiper .swiper", {
+  // Optional parameters
+  direction: "horizontal",
   slidesPerView: 1,
   spaceBetween: 10,
   loop: false,
@@ -9,63 +54,18 @@ const air_max_swiper = new Swiper("#air-max .swiper", {
   breakpoints: {
     // when window width is >= 576px
     576: {
-      slidesPerView: 2,
+      slidesPerView: 1,
       spaceBetween: 10,
     },
     // when window width is >= 768px
     768: {
-      slidesPerView: 3,
+      slidesPerView: 1,
       spaceBetween: 10,
     },
     // when window width is >= 992px
     992: {
-      slidesPerView: 3,
-      spaceBetween: 10,
-    },
-    // when window width is >= 1200px
-    1200: {
-      slidesPerView: 3.07,
-      spaceBetween: 10,
-    },
-    // when window width is >= 1400px
-    1400: {
-      slidesPerView: 3.5,
-      spaceBetween: 10,
-    },
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
-
-
-//gear-up-section-sliders:
-//1-men-swiper
-const gear_up_men_swiper = new Swiper("#men-swiper .swiper", {
-  // Optional parameters
-  direction: "horizontal",
-  slidesPerView: 1,
-  spaceBetween: 5,
-  loop: false,
-  grabCursor: true,
-  breakpoints: {
-    // when window width is >= 576px
-    576: {
       slidesPerView: 1,
-      spaceBetween: 5,
-    },
-    // when window width is >= 768px
-    768: {
-      slidesPerView: 1,
-      spaceBetween: 5,
-    },
-    // when window width is >= 992px
-    992: {
-      slidesPerView: 1,
-      spaceBetween: 5,
+      spaceBetween: 10,
     },
     // when window width is >= 1200px
     1200: {
@@ -85,31 +85,30 @@ const gear_up_men_swiper = new Swiper("#men-swiper .swiper", {
     prevEl: ".swiper-button-prev",
   },
 });
-
 
 //2-women-swiper
 const gear_up_women_swiper = new Swiper("#women-swiper .swiper", {
   // Optional parameters
   direction: "horizontal",
   slidesPerView: 1,
-  spaceBetween: 5,
+  spaceBetween: 10,
   loop: false,
   grabCursor: true,
   breakpoints: {
     // when window width is >= 576px
     576: {
       slidesPerView: 1,
-      spaceBetween: 5,
+      spaceBetween: 10,
     },
     // when window width is >= 768px
     768: {
       slidesPerView: 1,
-      spaceBetween: 5,
+      spaceBetween: 10,
     },
     // when window width is >= 992px
     992: {
       slidesPerView: 1,
-      spaceBetween: 5,
+      spaceBetween: 10,
     },
     // when window width is >= 1200px
     1200: {
@@ -129,3 +128,59 @@ const gear_up_women_swiper = new Swiper("#women-swiper .swiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+/////////////////////////////
+// usp-section-slider
+const usp_swiper = new Swiper("#usp-section .swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  slidesPerView: 1,
+  spaceBetween: 100,
+  loop: true,
+  autoplay: true,
+});
+usp_swiper.el.addEventListener("mouseover", function () {
+  usp_swiper.autoplay.stop();
+});
+usp_swiper.el.addEventListener("mouseleave", function () {
+  usp_swiper.autoplay.start();
+});
+
+///////////////////////////////
+//essentials-slider
+const essentials_swiper = new Swiper("#essentials .swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  slidesPerView: 1.2,
+  spaceBetween: 5,
+  loop: false,
+  breakpoints: {
+    // when window width is >= 576px
+    576: {
+      slidesPerView: 1.2,
+      spaceBetween: 5,
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 2.2,
+      spaceBetween: 5,
+    },
+    // when window width is >= 992px
+    992: {
+      slidesPerView: 2.2,
+      spaceBetween: 5,
+    },
+    // when window width is >= 1200px
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 5,
+    },
+    // when window width is >= 1400px
+    1400: {
+      slidesPerView: 3,
+      spaceBetween: 5,
+    },
+  },
+});
+
+/////////////////////////////
